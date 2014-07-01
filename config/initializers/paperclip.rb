13158874bd1d0e9f9a5a0b7ea8f94bb2b0bb201b
Rogério R. Alcântara <rogerio.alcantara@avatek.com.br>
2014-07-01 20:12:29 +0800

    One more attemption of fix HTTPS references...

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -2,6 +2,6 @@ Paperclip::Attachment.default_options.merge!(
   url:                  ':s3_domain_url',
   path:                 '/:class/:attachment/:id/:style/:filename',
   storage:              :s3,
-  s3_credentials:       Rails.configuration.aws,
-  :s3_protocol => 'https'
+  s3_protocol:          :https,
+  s3_credentials:       Rails.configuration.aws
 )
