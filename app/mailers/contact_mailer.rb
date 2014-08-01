179a53675785c5444262b4ddd445dcb9232807b4
roalcantara <rogerio.alcantara@avatek.com.br>
2014-08-01 16:42:45 +0800

    Contact Us: fixed.

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,12 @@
 class ContactMailer < ActionMailer::Base
 
-  default from: 'info@littlecs.com'
+  default from:'info@littlecs.com'
 
   def request_info name, from, message
     @name = name
     @from = from
     @message = message
-    mail to:'info@theoandco.net', subject:'New contact request'
+    mail to:'info@littlecs.com', subject:'New contact request'
   end
+
 end
