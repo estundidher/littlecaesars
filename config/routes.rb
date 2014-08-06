acaade3243b3dc92debba1c4a6af04d8a3154581
roalcantara <rogerio.alcantara@avatek.com.br>
2014-08-06 11:40:29 +0800

    Payment > Approved > Call print from client.

--- config/routes.rb
+++ config/routes.rb
@@ -93,10 +93,11 @@ Rails.application.routes.draw do
   get 'checkout_modal', to: 'checkout#modal', as: :checkout_modal
 
   resources :orders, only: [:create, :destroy, :show]
-  get 'checkout/:code',       to: 'orders#index',   as: :checkout
-  get 'orders/:code/update',  to: 'orders#update',  as: :order_update
-  get 'orders/:code/print',   to: 'orders#print',   as: :order_print
-  match 'checkout/confirm',   to: 'orders#confirm', as: :checkout_confirm, via: [:get, :post]
+  get 'checkout/:code',        to: 'orders#index',   as: :checkout
+  get 'orders/:code/update',   to: 'orders#update',  as: :order_update
+  get 'orders/:code/print',    to: 'orders#print',   as: :order_print
+  get 'checkout/:code/success',to: 'orders#success', as: :order_success
+  match 'checkout/confirm',    to: 'orders#confirm', as: :checkout_confirm, via: [:get, :post]
 
   controller :home do
     get 'shopping',          to: :shopping,     as: :shopping
