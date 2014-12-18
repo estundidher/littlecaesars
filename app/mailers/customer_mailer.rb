983c9d7c02121a96d64f591a9776bb7027204c33
bruiod <bru_iod@hotmail.com>
2014-12-18 13:01:00 +0800

    email confirmation message from Little Caesars to Little Cs

--- app/mailers/customer_mailer.rb
+++ app/mailers/customer_mailer.rb
@@ -5,7 +5,7 @@ class CustomerMailer < ActionMailer::Base
   def new_order order
     @order = order
     mail to:@order.customer.email,
-         subject:"Thank you for purchasing at Little Caesars'"
+         subject:"Thank you for purchasing at Little Cs"
   end
 
 end
