2c17acde8ada8d74c7cabd670d1ccb870b74c199
estundidher <estundidher13@gmail.com>
2015-03-26 18:00:13 +0800

    Reverting everything

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,4 +1,6 @@
-require 'aws-sdk'
+#require 'aws-sdk' It will load the v2 which causes assets compilation issues. Added by Diego
+require 'aws-sdk-v1'
+
 # Rails.configuration.aws is used by AWS, Paperclip, and S3DirectUpload
 Rails.configuration.aws = YAML.load(ERB.new(File.read("#{Rails.root}/config/aws.yml")).result)[Rails.env].symbolize_keys!
 AWS.config(logger: Rails.logger)
