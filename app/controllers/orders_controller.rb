bd587edd4ededa62ec56354b65de8b9a5c087ed8
estundidher <estundidher13@gmail.com>
2015-08-24 20:37:07 +0800

    Fixing redirect http protocol
    Fixing redirect http protocol

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,9 +12,9 @@ class OrdersController < ApplicationController
     return true
   end
 
-  before_filter :redirect_https, except: [:create, :success, :show, :print]
+  before_filter :redirect_https, except: [:create, :show, :print]
 
-  #before_filter :redirect_http, only: [:success]
+  before_filter :redirect_http, only: [:success] # It needs to be HTTP for the print
 
   skip_before_filter :verify_authenticity_token, only: [:confirm]
 
